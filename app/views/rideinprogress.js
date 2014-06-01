45841ba0817e11ec1e769088371de372944f0fc7
Christopher Lovejoy <c@lovejoy.info>
2014-06-01 03:58:30 -0700

    hook in background mode cordova plugin

--- app/views/rideinprogress.js
+++ app/views/rideinprogress.js
@@ -3,9 +3,20 @@ var RideinprogressView = Ember.View.extend({
   interval: 1000,
 
   didInsertElement: function() {
-    var __this = this;
+    var __this = this, confirm;
     this._super();
+
+    if ( window.plugin && window.plugin.backgroundMode ) {
+      confirm = true;
+      this.set('backgroundModeAvailable', true);
+      $('#feedback').addClass('alert-success').text( "The app will track your location in the background." );
+    } else {
+      $('#feedback').addClass('alert-warning').text( "The app cannot run in the background on this device." );
+      this.set('backgroundModeAvailable', false);
+    }
+
     this.startTracking();
+
     $('#end').click( function( event ) {
 
       event.preventDefault();
@@ -19,17 +30,28 @@ var RideinprogressView = Ember.View.extend({
 
     this.requestLocation( function() {
       var id;
+
       id = setInterval( function() {
         __this.reportLocation.apply( __this, arguments );
       }, __this.interval );
       __this.set( 'intervalID', id );
+
+
+    if ( this.get('backgroundModeAvailable') ) {
+      window.plugin.backgroundMode.enable();
+    }
+
     } );
   },
 
   stopTracking: function() {
     clearInterval( this.get('intervalID') );
 
-    $('#status').text( 'Ending the ride.' );
+    if ( this.get('backgroundModeAvailable') ) {
+      window.plugin.backgroundMode.disable();
+    }
+
+    $('#status'.text( 'Ending the ride.' ) );
 
     window.location = '/endride';
   },
