c75c6eb60545dce7006a45f2cf9d9d380975285d
Christopher Lovejoy <c@lovejoy.info>
2014-06-01 02:37:31 -0700

    ride in progress view

--- app/views/login.js
+++ app/views/login.js
@@ -2,6 +2,11 @@ var LoginView = Ember.View.extend({
   didInsertElement: function() {
     var __this = this;
     this._super();
+
+    __this.set('newUserElementsHidden', false);
+
+    __this.updateFormAction('immediate');
+
     this.$("#new-user-toggle").click(function(event) {
       event.preventDefault();
 
@@ -9,23 +14,34 @@ var LoginView = Ember.View.extend({
 
       console.log("toggle", __this.get("newUserElementsHidden"));
 
-      $(".new-user-only").each(function() {
-        if ( $(this).is('.form-group') ) {
-          $(this).slideToggle( __this.get("newUserElementsHidden") );
-        } else {
-          $(this).toggle( __this.get("newUserElementsHidden") );
-        }
-      });
-
-      $(".existing-user-only").each(function() {
-        if ( $(this).is('.form-group') ) {
-          $(this).slideToggle( ! __this.get("newUserElementsHidden") );
-        } else {
-          $(this).toggle( ! __this.get("newUserElementsHidden") );
-        }
-      });
+      __this.updateFormAction();
 
     });
+  },
+
+  updateFormAction: function(immediate) {
+    var __this = this, action;
+
+    action = ( __this.get("newUserElementsHidden") ? 'login' : 'signup' );
+
+    $('input[name="action"]').val( action );
+
+    $(".new-user-only").each(function() {
+      if ( $(this).is('.form-group') && !immediate ) {
+        $(this).slideToggle( __this.get("newUserElementsHidden") );
+      } else {
+        $(this).toggle( __this.get("newUserElementsHidden") );
+      }
+    });
+
+    $(".existing-user-only").each(function() {
+      if ( $(this).is('.form-group') && !immediate ) {
+        $(this).slideToggle( ! __this.get("newUserElementsHidden") );
+      } else {
+        $(this).toggle( ! __this.get("newUserElementsHidden") );
+      }
+    });
+
   }
 });
 
