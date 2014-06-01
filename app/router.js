fa791e944f9b4eaf32b3fdd3e553f5b06b61ccbf
Christa Hartsock <christa.hartsock@gmail.com>
2014-06-01 12:29:08 -0700

    add awesome fonts from font-awesome

--- app/router.js
+++ app/router.js
@@ -4,6 +4,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
     this.route("login", { path: "/login" });
+    this.route("signup", { path: "/signup" });
     this.route("whereto", { path: "/whereto" });
     this.route("similarriders", { path: "/similarriders" });
     this.route("messagerider", { path: "/messagerider" });
